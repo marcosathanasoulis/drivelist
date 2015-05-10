afb3175ffea62be83201bee77d993f350264b2b6
marcosathanasoulis <marcos@cloudfast.io>
2015-05-09 22:16:20 -0700

    Update win32.js

--- build/win32.js
+++ build/win32.js
@@ -7,7 +7,7 @@ _ = require('lodash');
 tableParser = require('table-parser');
 
 exports.list = function(callback) {
-  return childProcess.exec('wmic diskdrive get DeviceID, Caption, Size, Name', {}, function(error, stdout, stderr) {
+  return childProcess.exec('powershell.exe "Get-WmiObject -class Win32_Volume | Select-Object DeviceID,DriveLetter"', {}, function(error, stdout, stderr) {
     var result;
     if (error != null) {
       return callback(error);
@@ -17,16 +17,12 @@ exports.list = function(callback) {
     }
     result = tableParser.parse(stdout);
     result = _.map(result, function(row) {
-      var size, _ref;
-      size = _.parseInt((_ref = row.Size) != null ? _ref[0] : void 0) / 1e+9 || void 0;
       if (row.DeviceID.length > 1) {
         row.Caption = row.Caption.concat(_.initial(row.DeviceID));
       }
       return {
         device: _.last(row.DeviceID),
-        description: row.Caption.join(' '),
         name: row.Name,
-        size: size != null ? "" + (Math.floor(size)) + " GB" : void 0
       };
     });
     return callback(null, result);
